ba78aa4a838a68793de9d321c036c5dee51a92cd
Your Mama <mctraher@example.com>
2026-02-01 23:06:00 +0300

    add mongodb

---
+++ backend/src/consts.ts
@@ -0,0 +1,2 @@
+export const PORT = process.env.PORT || 3001;
+export const MONGO_DB_URL = `mongodb://${process.env['MONGO_INITDB_ROOT_USERNAME']}:${process.env['MONGO_INITDB_ROOT_PASSWORD']}@mongodb:27017/${process.env['MONGO_INITDB_DATABASE']}?authSource=${process.env['MONGO_INITDB_ROOT_USERNAME']}`
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import { MONGO_DB_URL, PORT } from './consts.js';
 
 const app = express();
-const port = Number(process.env.PORT) || 3001;
+
+mongoose.connect(MONGO_DB_URL);
 
 app.get('/', (request, response) => {
   response.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
